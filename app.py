dd453a44622295c21b76972a38e6d9ba8e6505a8
fran <jane08150020@163.com>
2023-01-12 13:50:08 -0800

    fix flash msg and revise conditions. Code works.

--- app.py
+++ app.py
@@ -21,18 +21,17 @@ def convert():
     target_currency = request.form['target-currency']
     target_symbol = CurrencyConverter.get_symbol(target_currency)
     amount = int(request.form["amount"])
-    # convert_amount = 0
 
-    if CurrencyCodes().get_currency_name(source_currency) is None:
-        flash('source currency not valid', 'error')
-        redirect('/')
-    if CurrencyCodes().get_currency_name(target_currency) is None:
-        flash('target currency not valid', 'error')
-        redirect('/')
     try:
-        convert_amount = f"{CurrencyConverter.convert(source_currency, target_currency, amount):.2f}"
-        flash('The result is here', 'success')
+        if CurrencyCodes().get_currency_name(source_currency) is None:
+            flash('Source currency not valid', 'error')
+            return render_template('index.html')
+        if CurrencyCodes().get_currency_name(target_currency) is None:
+            flash('Target currency not valid', 'error')
+            return render_template('index.html')
+        else:
+            convert_amount = f"{CurrencyConverter.convert(source_currency, target_currency, amount):.2f}"
+            flash(f'The result is {target_symbol} {convert_amount}', 'success')
+            return render_template('index.html')
     except RatesNotAvailableError:
         flash('There\'s no valid rate', 'error')
-    finally:
-        return render_template("output.html", target_symbol=target_symbol, convert_amount=convert_amount)
